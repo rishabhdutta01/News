199c419b5cced9ec564cefc38abfd84277081ece
rishabhdutta01 <rishabh.dutta01@gmail.com>
2020-11-27 17:29:49 +0530

    starting

---
+++ backend/newsapi/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()\
+
+@app.get("/pets/")
+async def get_all_pets():
+    """[summary]
+    Gets all pets adopted/rescued or not.    [description]
+    Endpoint for all pets.
+    """
+    fake_pets = [{"name": "Guero"}, {"name": "Peppy"}]
+    return fake_pets
